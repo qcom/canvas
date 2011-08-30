d4b155aefa3ad0b31de9253fa96521b98687c03e
Zach <qcom100@gmail.com>
2011-08-30 12:25:51 -0700

    script.js

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,4 @@
-/*$(document).ready(function(){
+$(document).ready(function(){
   var socket = io.connect();
 
   var canvas = document.getElementById('canvas');
@@ -29,57 +29,19 @@
 	    return ctx.closePath();
     }
   }
-});*/
-(function() {
-  var App;
-  App = {};
-  /*
-  	Init 
-  */
-  App.init = function() {
-    App.canvas = document.getElementById('canvas');
-    App.canvas.height = 400;
-    App.canvas.width = 800;
-    App.ctx = App.canvas.getContext("2d");
-    App.ctx.fillStyle = "solid";
-    App.ctx.strokeStyle = "#ECD018";
-    App.ctx.lineWidth = 5;
-    App.ctx.lineCap = "round";
-    App.socket = io.connect();
-    App.socket.on('draw', function(data) {
-      return App.draw(data.x, data.y, data.type);
-    });
-    App.draw = function(x, y, type) {
-      if (type === "dragstart") {
-        App.ctx.beginPath();
-        return App.ctx.moveTo(x, y);
-      } else if (type === "drag") {
-        App.ctx.lineTo(x, y);
-        return App.ctx.stroke();
-      } else {
-        return App.ctx.closePath();
-      }
-    };
-  };
-  /*
-  	Draw Events
-  */
-  $('canvas').live('drag dragstart dragend', function(e) {
-    var offset, type, x, y;
-    type = e.handleObj.type;
-    offset = $(this).offset();
-    e.offsetX = e.layerX - offset.left;
-    e.offsetY = e.layerY - offset.top;
-    x = e.offsetX;
-    y = e.offsetY;
-    App.draw(x, y, type);
-    App.socket.emit('drawClick', {
-      x: x,
-      y: y,
-      type: type
-    });
+
+  $('#canvas').live('drag dragstar dragend', function(e){
+	  var type = e.handleObj.type;
+	  var offset = $(this).offset();
+	  var e.offsetX = e.layerX - offset.left;
+	  var.offsetY = e.layerY - offset.top;
+	  var x = e.offsetX;
+	  var y = e.offsetY;	
+	  draw(x, y, type);
+	  socket.emit('drawClick', {
+		  x: x,
+		  y: y,
+		  type: type
+  	});
   });
-  $(function() {
-    return App.init();
-  });
-}).call(this);
+});
